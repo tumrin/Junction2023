46ebeb4d65501503f30ace2d45bb1a22403876dd
Nevil Sandaradura <nevil.sandaradura@gmail.com>
2023-11-11 01:39:08 +0200

    improved request

--- web/src/components/profile.rs
+++ web/src/components/profile.rs
@@ -1,8 +1,7 @@
 use leptos::{*, leptos_dom::logging::console_log};
-use reqwasm::Error;
+use serde::Deserialize;
 
-
-#[derive(Clone, PartialEq, Debug)]
+#[derive(Clone, PartialEq, Debug, Deserialize)]
 struct Profile{
     id: String,
     username: String,
@@ -13,36 +12,89 @@ struct Profile{
 
 #[component]
 pub fn Profile() -> impl IntoView {
-    let show_info = create_rw_signal(false);
-    let profile = create_rw_signal::<Option<Profile>>(None);
-    create_local_resource(profile, fetch_profile);
-    view! {
-        <div class="profile">
-            <span>Username</span>
-            <span>Likes</span>
-            <span>Current task</span>
-        </div>
+    let user_id = create_rw_signal("654e83471029b952630a92dd");
+    let profile = create_local_resource(move|| user_id.get(), fetch_profile);
+
+    console_log(&format!("profile: {:?}", profile));
+
+    move||{
+        if let Some(profile) = profile.get()  {
+            match profile {
+                Ok(profile) => {
+                    view! {
+                        <div class="profile">
+                            <span>{profile.username}</span>
+                            <span>likes: {profile.liked_cards.len()}</span>
+                            <span>{profile.in_progress}</span>
+                        </div>
+                    }
+                }
+                Err(_e) => view! { <div>{"Error loading profile"}</div> },
+                
+            }
+        }else {
+            view! { <div class="profile"></div> }
+        }
     }
 }
 
 
-async fn fetch_profile(_: Option<Profile>) -> Result<Profile, Error> {
-    let res = reqwasm::http::Request::get("http://localhost:3000/api/user/654e83471029b952630a92dd")
-        .send()
-        .await
-        .unwrap();
-    
-    let json = res.json::<Profile>().await.unwrap();
-    console_log(&format!("{:?}", res));
- 
+async fn fetch_profile(user_id: &str) -> Result<Profile, error::Error> {
+    console_log("fetching profile");
+    let res: Profile =
+    reqwasm::http::Request::get(&format!("http://127.0.0.1:3000/api/user/{user_id}"))
+    .send()
+    .await?
+    .json()
+    .await?;
+    console_log(&format!("profile: {:?}", res));
 
-    Ok(Profile {
-        id: "".to_string(),
-        username: "".to_string(),
-        in_progress: "".to_string(),
-        liked_cards: vec![],
-    })
+
+    Ok(res)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
