c118f358961444970f97b056f5cb219745a4adf9
Tuomas Rinne <tuomas.rin@hotmail.com>
2023-11-10 20:03:14 +0200

    add reqwest

---
+++ web/src/components/card.rs
@@ -0,0 +1,12 @@
+use leptos::*;
+
+#[component]
+pub fn Card() -> impl IntoView {
+    view! {
+        <div class="card">
+            <h1>Title</h1>
+            <p>asdf</p>
+        </div>
+    }
+}
+
--- web/src/main.rs
+++ web/src/main.rs
@@ -1,15 +1,16 @@
 use leptos::*;
 
+use crate::components::card::Card;
+
+mod components;
+mod pages;
+
 fn main() {
     mount_to_body(App)
 }
 
 #[component]
 fn App() -> impl IntoView {
-    view! {
-        <div>
-            <p>asdfsa</p>
-        </div>
-    }
+    view! { <Card/> }
 }
 
