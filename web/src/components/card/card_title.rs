a1dcb85eaa38c6c71dd3d7103bf59a7d08f55f44
Tuomas Rinne <tuomas.rin@hotmail.com>
2023-11-11 19:34:47 +0200

    video style

--- web/src/components/card/card_title.rs
+++ web/src/components/card/card_title.rs
@@ -5,10 +5,13 @@ use crate::components::card::buttons::Buttons;
 #[component]
 pub fn CardTitle(title: String, video: String) -> impl IntoView {
     view! {
-        <div class="card-title">
-            <h1>{title}</h1>
-            <h2>{video}</h2>
-            <Buttons/>
-        </div>
+
+            <div class="card-title">
+                <h1>{title.clone()}</h1>
+            <div class="video-wrapper">
+                <video loop=true autoplay=true src=format!("http://127.0.0.1:3000/api/video/{video}") />
+            </div>
+                <Buttons/>
+            </div>
     }
 }
