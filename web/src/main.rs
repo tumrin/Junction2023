fcef261bfb75be7c9cb08e6c68c80cb93ba21f17
Tuomas Rinne <tuomas.rin@hotmail.com>
2023-11-11 17:28:27 +0200

    create user and save to localhost

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,11 +1,16 @@
-use leptos::*;
+use leptos::{leptos_dom::logging::console_log, *};
 use leptos_use::storage::use_local_storage;
-use web_sys::js_sys::Math::random;
+use serde::Deserialize;
 
 use crate::components::card::Card;
 
 mod components;
 mod pages;
+
+#[derive(Deserialize)]
+struct User {
+    id: String,
+}
 
 fn main() {
     mount_to_body(App)
@@ -15,9 +20,21 @@ fn main() {
 fn App() -> impl IntoView {
     let card = create_local_resource(move || (), fetch_card);
     let (user_id, set_user_id, _) = use_local_storage("user", None);
-    if user_id.get_untracked().is_none() {
-        set_user_id(Some((random() * 1000.0).round()));
-    }
+    let user_request = create_rw_signal(None);
+
+    create_effect(move |_| {
+        if user_id.get().is_none() {
+            user_request.set(Some(create_local_resource(move || (), generate_user)));
+        }
+    });
+
+    create_effect(move |_| {
+        if let Some(user_request) = user_request.get() {
+            if let Some(Ok(user_id)) = user_request.get() {
+                set_user_id(Some(user_id))
+            }
+        }
+    });
 
     move || {
         // is some when request completes
@@ -40,6 +57,16 @@ fn App() -> impl IntoView {
         }
     }
 }
+async fn generate_user(_: ()) -> Result<String, error::Error> {
+    let res: String = reqwasm::http::Request::get("http://127.0.0.1:3000/api/user/create")
+        .send()
+        .await?
+        .json::<User>()
+        .await?
+        .id;
+    Ok(res)
+}
+
 async fn fetch_card(_: ()) -> Result<Card, error::Error> {
     let res: Card = reqwasm::http::Request::get("http://127.0.0.1:3000/api/card")
         .send()
