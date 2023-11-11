d5790e30e56fe9b49400ca26d8f7ad3c6eee5459
Tuomas Rinne <tuomas.rin@hotmail.com>
2023-11-11 14:56:25 +0200

    initial card scrolling

--- web/src/main.rs
+++ web/src/main.rs
@@ -11,5 +11,32 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    view! {<div class="app"> <Card/> </div>}
+    let card = create_local_resource(move || (), fetch_card);
+    move || {
+        // is some when request completes
+        if let Some(card_res) = card.get() {
+            // Check if request was succesfull
+            match card_res {
+                Ok(card_res) => {
+                    view! {<div class="app">
+                        <button on:click=move |_e| card.refetch()>previous</button>
+                        <Card card=card_res/>
+                        <button on:click=move |_e| card.refetch()>next</button>
+                        </div>
+                    }
+                }
+                Err(_e) => view! { <div>{"Error loading card"}</div>},
+            }
+        } else {
+            view! {<div class="card"></div>}
+        }
+    }
+}
+async fn fetch_card(_: ()) -> Result<Card, error::Error> {
+    let res: Card = reqwasm::http::Request::get("http://127.0.0.1:3000/api/card")
+        .send()
+        .await?
+        .json()
+        .await?;
+    Ok(res)
 }
