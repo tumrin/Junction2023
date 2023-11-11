5e23870c282a43980311e54bf12b60d2f888498b
Tuomas Rinne <tuomas.rin@hotmail.com>
2023-11-11 23:48:55 +0200

    in progress works

--- web/src/components/card/buttons.rs
+++ web/src/components/card/buttons.rs
@@ -1,12 +1,11 @@
-use crate::wasm_bindgen::JsValue;
-use leptos::leptos_dom::logging::console_log;
+use std::rc::Rc;
+
 use leptos::{component, create_rw_signal, spawn_local, use_context, IntoView};
 use leptos::{view, SignalGet};
 use leptos_icons::BiIcon::BiLikeSolid;
 use leptos_icons::BsIcon::BsRocketTakeoffFill;
 use leptos_icons::Icon;
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 
 use crate::{User, UserContext};
 
@@ -27,8 +26,11 @@ pub fn Buttons(card: String) -> impl IntoView {
     let user = use_context::<UserContext>().unwrap().0;
     let card = create_rw_signal(card);
 
+    let user = Rc::new(user);
+
     move || {
         if let Some(user) = user.get() {
+            let user1 = user.clone();
             view! {
                 <div class="buttons">
                     <div
@@ -39,7 +41,11 @@ pub fn Buttons(card: String) -> impl IntoView {
                     >
                         <Icon icon=Icon::from(BiLikeSolid) width="2em" height="2em"/>
                     </div>
-                    <div class="button">
+                    <div class="button"
+                                on:click=move |e| {
+                                start_request(&user1, card.get())
+                        }
+                >
                         <Icon icon=Icon::from(BsRocketTakeoffFill) width="2em" height="2em"/>
                     </div>
                 </div>
@@ -50,6 +56,22 @@ pub fn Buttons(card: String) -> impl IntoView {
     }
 }
 
+fn start_request(user: &User, card: String) {
+    let id = user.id.to_string();
+    let user = user.clone();
+    spawn_local(async move {
+        let body = UserInfoPutRequest {
+            inProgress: card,
+            likedCards: Some(user.likedCards.unwrap_or(vec![])),
+        };
+        reqwasm::http::Request::put(&format!("http://localhost:3000/api/user/{id}",))
+            .header("Content-Type", "application/json")
+            .body(serde_json::to_value(body).unwrap().to_string())
+            .send()
+            .await
+            .unwrap();
+    })
+}
 fn like_request(user: &User, card: String) {
     let id = user.id.to_string();
     let user = user.clone();
